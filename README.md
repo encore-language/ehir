2afcfb26f13e858f71d8a194a21f7a8d60e6a48e
Maksim Shushkevich <m.e.shushkevich@yandex.com>
2026-01-05 00:08:18 +0300

    Some fixes

--- README.md
+++ README.md
@@ -1,23 +1,2 @@
 # ehir
 Encore High Intermediate Representation
-
-## Roadmap
-
-### Version nightly-0.0.0
-- [ ] Memory management
-- [ ] Primitive types
-- [ ] Capture primitives
-- [ ] Control flow
-- [ ] Raw functions
-- [ ] Raw structs
-
-### Version nightly-0.0.1
-- [ ] Import instructions
-
-### Version nightly-0.0.2
-- [ ] Generics
-
-### Version nightly-0.0.3
-- [ ] Safe structs
-- [ ] Safe pointers
-- [ ] Auto deallocator
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -323,7 +323,7 @@ class Parser:
     def _safe_consume(self, expected: type[t.Token]) -> t.Token:
         current_token = self._consume()
         if not isinstance(current_token, expected):
-            raise ValueError(f"Expected {expected}, got {current_token}")
+            self._trace_unexpected_token(current_token, expected)
         return current_token
 
     def _consume(self) -> t.Token:
@@ -333,3 +333,6 @@ class Parser:
 
     def _is_at_end(self, n: int = 0) -> bool:
         return self._consumed + n >= len(self._tokens)
+
+    def _trace_unexpected_token(self, token: t.Token, expected_t: type[t.Token]):
+        raise ValueError(f"Unexpected token: {token}")
